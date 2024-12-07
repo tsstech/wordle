f41377f4803f71e44ee44cea2cb54748aacdd8da
tsstech <vidisha156@yahoo.com>
2024-12-07 17:04:17 +0000

    Created screen in main.py

---
+++ main.py
@@ -0,0 +1,14 @@
+import pygame, random
+import box_class
+
+############# GAME SETUP #################
+ 
+## Initialize program
+pygame.init()
+ 
+## create screen
+screen = pygame.display.set_mode((420,600))
+
+## title
+pygame.display.set_caption("Wordle")        # displaying title
+
